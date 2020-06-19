7b7866c625e92803be8b8c4c96d679840fb2ac82
Lai Yen Ju <yenju.lai@icloud.com>
2020-06-19 10:46:56 +0800

    hw1, hw2 finished

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,12 @@
 function printStars(n) {
-  
+  var stars = "";
+  for(var i=0; i<n; i++){
+    stars = stars + "*";
+  }
+  console.log(stars);
 }
 
+printStars(1);
+printStars(3);
 printStars(5);
+printStars(6);
--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,22 @@
 function capitalize(str) {
-  
-}
+  var newString = "";
+
+  if (str.charCodeAt(0)>=65 && str.charCodeAt(0)<=122) {
+    if(str.charCodeAt(0)>=97 && str.charCodeAt(0)<=122) {
+      var exchanged = String.fromCharCode(str.charCodeAt(0));
+      var following = str.slice(1);
+      newString = exchanged + following;
+
+      return(newString);
+    } else {
+      return(str);
+    }
+  } else {
+    return(str);
+  }
+};
 
 console.log(capitalize('hello'));
+console.log(capitalize('nick'));
+console.log(capitalize('Nick'));
+console.log(capitalize(',hello'));
