43bfdfc92f5ddb5fc4db659d8d575bbd5df39fde
Lai Yen Ju <yenju.lai@icloud.com>
2020-07-11 23:52:07 +0800

    fix hw2 eslint

--- homeworks/week4/hw2.js
+++ homeworks/week4/hw2.js
@@ -1,83 +1,86 @@
+/* eslint-disable consistent-return */
 const request = require('request');
 const process = require('process');
-// 參考答案內將 base url、json 陣列位置個別設定變數，這個好處是維護方便，之後會試著這樣做。
-// 關於分情況處理的寫法，參考答案使用 switch case 鎖定輸入值，用來區分情況
 
-// 輸出前 20 本書
-if (process.argv[2] === 'list') {
-  request(
-    'https://lidemy-book-store.herokuapp.com/books?_limit=20',
-    (error, response, body) => {
-      if (error) {
-        console.log('抓取失敗', error);
-      }
-      const json = JSON.parse(body);
-      for (let i = 0; i < 20; i += 1) {
-        console.log(json[i].id, json[i].name);
-      }
-    },
-  );
+const API_ENDPOINT = 'https://lidemy-book-store.herokuapp.com';
+const action = process.argv[2];
+const params = process.argv[3];
+
+function listBooks() {
+  request(`${API_ENDPOINT}/books?_limit=20`, (err, res, body) => {
+    if (err) {
+      return console.log('抓取失敗', err);
+    }
+    const data = JSON.parse(body);
+    for (let i = 0; i < data.length; i += 1) {
+      console.log(`${data[i].id} ${data[i].name}`);
+    }
+  });
 }
 
-// 輸出特定 id 的書目
-if (process.argv[2] === 'read') {
-  request(
-    `https://lidemy-book-store.herokuapp.com/books/${process.argv[3]}`,
-    (error, response, body) => {
-      if (error) {
-        console.log('抓取失敗', error);
-      }
-      const json = JSON.parse(body);
-      console.log(json.id, json.name);
-    },
-  );
+function readBook(id) {
+  request(`${API_ENDPOINT}/books/${id}`, (err, res, body) => {
+    if (err) {
+      return console.log('抓取失敗', err);
+    }
+    const data = JSON.parse(body);
+    console.log(data);
+  });
 }
 
-// 刪除書目
-if (process.argv[2] === 'delete') {
-  request.delete(
-    `https://lidemy-book-store.herokuapp.com/books/${process.argv[3]}`,
-    (error, response) => {
-      if (error) {
-        console.log('刪除失敗', error);
-      }
-      console.log(response.statusCode, '刪除成功');
-    },
-  );
+function deleteBook(id) {
+  request.delete(`${API_ENDPOINT}/books/${id}`, (err) => {
+    if (err) {
+      return console.log('刪除失敗', err);
+    }
+    console.log('刪除成功！');
+  });
 }
 
-// 新增書目
-if (process.argv[2] === 'create') {
-  request.post(
-    {
-      url: 'https://lidemy-book-store.herokuapp.com/books',
-      form: {
-        name: `${process.argv[3]}`,
-      },
+function createBook(name) {
+  request.post({
+    url: `${API_ENDPOINT}/books`,
+    form: {
+      name,
     },
-    (error, response, body) => {
-      if (error) {
-        console.log('新增失敗', error);
-      }
-      console.log(body, '新增成功');
-    },
-  );
+  }, (err, res, body) => {
+    if (err) {
+      return console.log('新增失敗', err);
+    }
+    console.log(body, '新增成功！');
+  });
 }
 
-// 修改書目
-if (process.argv[2] === 'update') {
-  request.patch(
-    {
-      url: `https://lidemy-book-store.herokuapp.com/books/${process.argv[3]}`,
-      form: {
-        name: `${process.argv[4]}`,
-      },
+function updateBook(id, name) {
+  request.patch({
+    url: `${API_ENDPOINT}/books/${id}`,
+    form: {
+      name,
     },
-    (error, response, body) => {
-      if (error) {
-        console.log('更新失敗', error);
-      }
-      console.log(body, '更新成功');
-    },
-  );
+  }, (err, res, body) => {
+    if (err) {
+      return console.log('更新失敗', err);
+    }
+    console.log(body, '更新成功！');
+  });
+}
+
+switch (action) {
+  case 'list':
+    listBooks();
+    break;
+  case 'read':
+    readBook(params);
+    break;
+  case 'delete':
+    deleteBook(params);
+    break;
+  case 'create':
+    createBook(params);
+    break;
+  case 'update':
+    updateBook(params, process.argv[4]);
+    break;
+  default:
+    console.log('Available commands: list, read, delete, create and update');
 }
