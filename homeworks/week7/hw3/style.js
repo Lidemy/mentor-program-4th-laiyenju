18f8f1621620ec04384b74b82d992c2f837c5d85
Lai Yen Ju <yenju.lai@icloud.com>
2020-08-12 14:33:37 +0800

    fix week7 hw1 bugs after watching review video

--- homeworks/week7/hw3/style.js
+++ homeworks/week7/hw3/style.js
@@ -3,11 +3,23 @@
 const input = document.querySelector('#input__text');
 const list = document.querySelector('.list__section');
 
+// 為安全性而生的跳脫字串
+function escapeHtml(unsafe) {
+  return unsafe
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
+}
+
 // 監聽文字輸入框，按下 Enter 鍵就新增一個 list item
 input.addEventListener('keypress', function (e) {
   if (e.key === 'Enter') {
     // listContent 是輸入的文字
     let listContent = input.value;
+    // 如果文字輸入欄是空白，就算 enter 送出也無法新增項目
+    if (!input.value) return;
     // 新增列表，並使用 insertBefore() 將輸入文字塞入列表的開頭
     const listItem = document.createElement('div');
     list.insertBefore(listItem, list.childNodes[0]);
@@ -15,7 +27,7 @@ input.addEventListener('keypress', function (e) {
     <div class="list__content">
 
       <input type="checkbox">
-      <label class="list__text">${listContent}</label>
+      <label class="list__text">${escapeHtml(listContent)}</label>
       <span class="material-icons remove">delete_outline</span>
     </div>
     `;
@@ -24,9 +36,9 @@ input.addEventListener('keypress', function (e) {
   }
 });
 
-
-// 用 click 搭配 event delegation 方式監聽父層 list，for loop 檢查 checkbox 是否有被勾選()，如果 checkbox 被勾選，要把項目中的文字劃掉。
-list.addEventListener('click', function () {
+// 用 click 搭配 event delegation 方式監聽父層 list，for loop 檢查 checkbox 是否有被勾選()、檢查是否有按到刪除鈕。
+// 如果 checkbox 被勾選，要把項目中的文字劃掉。如果按了刪除紐，要把項目刪掉。
+list.addEventListener('click', function (e) {
   // 偵測到 click 後才抓取 checkbox
   const checkboxItems = document.querySelectorAll('input[type=checkbox]');
   for (let checkboxItem of checkboxItems) {
@@ -36,10 +48,8 @@ list.addEventListener('click', function () {
       checkboxItem.parentNode.classList.remove('finish');
     }
   }
-});
 
-// 監聽刪除鍵，使用 e.target 監測 click 到的元件是什麼，使用 e.target.classList.contain() 檢查是不是刪除鍵
-list.addEventListener('click', function (e) {
+  // 使用 e.target 監測 click 到的元件是什麼，使用 e.target.classList.contain() 檢查是不是刪除鍵
   // 因為刪除鍵設定的 class 是 remove，如果點選到的元件擁有 remove class，就確認是刪除鍵，可以刪除此項目。
   if (e.target.classList.contains('remove')) {
     e.target.parentNode.remove('e.target');
